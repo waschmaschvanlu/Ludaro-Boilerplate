24d204447d6be1b179feedf4a3e8a07159ea016b
ludaro <92580232+waschmaschvanlu@users.noreply.github.com>
2023-03-04 01:25:24 +0100

    Add files via upload

---
+++ server/functions.lua
@@ -0,0 +1,18 @@
+if (GetResourceState("es_extended") == "started") then
+    if (exports["es_extended"] and exports["es_extended"].getSharedObject) then
+        ESX = exports["es_extended"]:getSharedObject()
+    else
+        TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
+    end
+end
+
+
+function debug(msg)
+    if Config.Debug then
+        if type(msg) == "table" then
+            print(print(ESX.DumpTable(msg)))
+        else
+            print("[Ludaro|Debug] : " .. tostring(msg))
+        end
+    end
+end
